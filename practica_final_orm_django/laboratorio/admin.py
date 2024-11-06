83d3646da79dffb4bb04fac0fc9ca4040b4f107e
Phermutz <phermutz@gmail.com>
2024-11-06 12:30:55 -0300

    admin y visualizacion plantillas html, base.html

--- practica_final_orm_django/laboratorio/admin.py
+++ practica_final_orm_django/laboratorio/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Producto, Laboratorio, DirectorGeneral
 
-# Register your models here.
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'laboratorio', 'anio_fabricacion', 'precio_costo', 'precio_venta')
+    list_filter = ('laboratorio',)
+
+@admin.register(Laboratorio)
+class LaboratorioAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'ciudad', 'pais')
+
+@admin.register(DirectorGeneral)
+class DirectorGeneralAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'laboratorio')
