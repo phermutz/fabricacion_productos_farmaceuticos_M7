a5948b5fe2add40370664b52570b7de524351d76
Phermutz <phermutz@gmail.com>
2024-11-06 11:47:43 -0300

    Configuración inicial, settings.py postgres de la aplicación laboratorio models.py y migraciones

--- practica_final_orm_django/laboratorio/models.py
+++ practica_final_orm_django/laboratorio/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Laboratorio(models.Model):
+    nombre = models.CharField(max_length=100)
+    ciudad = models.CharField(max_length=100, blank=True, null=True)
+    pais = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+
+class DirectorGeneral(models.Model):
+    nombre = models.CharField(max_length=100)
+    laboratorio = models.OneToOneField(Laboratorio, on_delete=models.CASCADE, related_name='director_general')
+    especialidad = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.nombre} ({self.laboratorio.nombre})"
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    laboratorio = models.ForeignKey('Laboratorio', on_delete=models.CASCADE)
+    anio_fabricacion = models.IntegerField()
+    precio_costo = models.DecimalField(max_digits=10, decimal_places=2)
+    precio_venta = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.nombre
